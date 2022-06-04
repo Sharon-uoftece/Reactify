48d831ad3b93f5903d736a12b34f90c579400632
Siyuan sharon Li <sharonsiyuan.li@mail.utoronto.ca>
2022-06-04 07:12:06 -0400

    june4 new

--- reactify/src/index.js
+++ reactify/src/index.js
@@ -1,27 +1,5 @@
-import React, {Component} from "react"
+import React from "react"
 import ReactDOM from "react-dom"
+import App from "./App"
 
-class Index extends Component {
-    constructor(){
-        super()
-        this.state = {
-            isLoggedIn: false
-        }
-    }
-
-    render() {
-        let status = "undefined"
-        if (this.state.isLoggedIn === true) {
-            status = "in"
-        } else {
-            status = "out"
-        }
-
-        return(
-            <div>
-                <h1>{status}</h1>
-            </div>
-        )
-    }
-}
-ReactDOM.render(<Index />,document.getElementById("root"))
+ReactDOM.render(<App/>, document.getElementById("root"))
